3c2a277834f93a72b855a91385fc1d04e7f89eee
ziabaub <ziabaub@gmail.com>
2019-04-18 12:48:36 +0300

    done the file uploading

--- services.php
+++ services.php
@@ -10,32 +10,10 @@
 require("tools.php");
 $fileTmpName = "";
 if (!empty($_POST["name"])) {
-    //   error(3);
-    $text = readFromFile("needItText.txt");
-    $title = getUrl($_POST);
-    $data = sortPost($_POST);
-    $fileTmpName = "";
-} else if (isset($_POST["search"])) {
-    if ($_POST["search"] != "!!!") {
-        $data = search($_POST["search"]);
-    }else{
-    $data ="";
-    $title = "Search Engine";}
-} else if (isset($_POST["getDatabase"])) {
-    $title = "All Data";
-    $data = getInformation();
-} else if (isset($_GET["saveData"])) {
-    $title = "Saved ";
-    $data = "Data has been saved success";
-    writeIntoFile($_GET["pathName"],$_GET["data"]);
-} else if (isset($_POST["upload"])) {
-    $title = "Data From File";
-    $data = getDataFromUploadedFile($_POST);
-    $fileTmpName =getFilePath($_POST);
-} else {
-    $title = "Add Your Company";
-    $data = "<h1>NO DATA!!!</h1>";
 
+$title = checkSpelling($_POST);
+}else {
+    $title = "Add Your Data";
 }
 ?>
 <!DOCTYPE html>
@@ -55,8 +33,6 @@ if (!empty($_POST["name"])) {
             <li>Address:</li>
             <li>Phone:</li>
             <li>Email:</li>
-            <li><h3>Search E.</h3></li>
-            <li><h4>C. Name</h4></li>
         </ul>
     </div>
 
@@ -65,61 +41,23 @@ if (!empty($_POST["name"])) {
         <form action="services.php?name=<?php echo $title ?>" method="post">
             <ul>
                 <li>
-                    <input type="text" name="name">
+                    <input type="text" name="name" onfocus="this.value =''" value="Ziad Sarrih" >
                 </li>
                 <li>
-                    <input type="text" name="address">
+                    <input type="text" name="address" onfocus="this.value =''" value="Country Town Street 345">
                 </li>
                 <li>
-                    <input type="text" name="phone">
+                    <input type="text" name="phone" onfocus="this.value =''" value="+375444722956">
                 </li>
                 <li>
-                    <input type="text" name="email">
+                    <input type="text" name="email" onfocus="this.value =''" value="zio@info.com">
                 </li>
                 <li>
                     <input type="submit" name="Submit">
                 </li>
-                <li>
-                    <form action="services.php?name=<?php echo $title ?>" method="post">
-                        <input type="submit" name="getDatabase" value="GetDataBase">
-                    </form>
-                </li>
-                <li>
-                    <form action="services.php?name=<?php echo $title ?>" method="post">
-                        <ul class="secondUl ">
-                            <li>
-                                <input class="search_input" type="text" name="search" onfocus="this.value =''"
-                                       value="!!!">
-                            </li>
-                            <li>
-                                <input type="submit" name="Submit_Search" value="Search">
-                            </li>
-                        </ul>
-                    </form>
-                </li>
-                <li>
-                    <form action="services.php?name=<?php echo $title ?>" method="post" enctype="multipart/form-data">
-                        Upload a File:<br/>
-                        <input type="file" name="myfile" id="fileToUpload">
-                        <input type="submit" name="upload" value="Upload File Now">
-                    </form>
-                </li>
+
             </ul>
         </form>
-    </div>
-
-    <div class="textAreaBlock">
-        <div id="product_list" class="textArea" contenteditable="true"><br>
-            <?php echo $data;
-            ?>
-        </div>
-        <div><br/>
-            <form action="EditData.php?name=<?php echo $title ?>" method="get">
-                <input type="hidden" name="varName" value='<?php echo $data ?>'>
-                <input type="hidden" name="pathName" value='<?php echo $fileTmpName ?>'>
-                <input type="submit" name="saveData" value="save"/>
-            </form>
-        </div>
     </div>
 
 </div>
